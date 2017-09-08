b30e549b2d09866a9d0f31f711b27cb8a32765b9
Olav Hobberstad <colddimension@gmail.com>
2017-09-08 22:11:43 +0200

    updated parser

--- olav/pick.py
+++ olav/pick.py
@@ -1,8 +1,12 @@
 RESULTS_FILE = 'data/results.txt'
+WINNER_FILE = 'data/best.txt'
 BEST_RESULT = {}
+BEST_MAP = 0
+BEST_MRR = 0
+BEST_P10 = 0
 
 
-def compare(a, b):
+def compare_all(a, b):
     if b:
         cont = a[list(a)[0]]
         best = b[list(b)[0]]
@@ -14,6 +18,13 @@ def compare(a, b):
         return a
 
 
+def compare_one(a, b):
+    if float(a) > float(b):
+        return a
+    else:
+        return b
+
+
 def isfloat(value):
     try:
         float(value)
@@ -23,7 +34,7 @@ def isfloat(value):
 
 
 def read_data():
-    global BEST_RESULT
+    global BEST_RESULT, BEST_MRR, BEST_MAP, BEST_P10
     with open(RESULTS_FILE, 'r') as rfile:
         lines = [x.strip('\n') for x in rfile.readlines()]
         params = []
@@ -36,10 +47,16 @@ def read_data():
                 elements = line.split('  ')
                 temp_res[elements[0]] = [elements[i] for i in range(1, 4)]
                 temp_res['params'] = ['{:05.3f}'.format(param) for param in params]
-                BEST_RESULT = compare(temp_res, BEST_RESULT)
+                BEST_RESULT = compare_all(temp_res, BEST_RESULT)
+                BEST_P10 = compare_one(temp_res[elements[0]][0], BEST_P10)
+                BEST_MAP = compare_one(temp_res[elements[0]][1], BEST_MAP)
+                BEST_MRR = compare_one(temp_res[elements[0]][2], BEST_MRR)
 
     return lines
 
 if __name__ == '__main__':
     read_data()
-    print(BEST_RESULT)
+    print('Best:\nResult: {0}\nP@10: {1}\nMAP: {2}\nMRR: {3}'.format(BEST_RESULT, BEST_P10, BEST_MAP, BEST_MRR))
+    with open(WINNER_FILE, 'w') as wfile:
+        print('Writing results to file.')
+        wfile.write('Result: {0}\nP@10: {1}\nMAP: {2}\nMRR: {3}'.format(BEST_RESULT, BEST_P10, BEST_MAP, BEST_MRR))
