0243be8f3b8165768988497695f7e21482b86109
Olav Hobberstad <colddimension@gmail.com>
2017-09-09 16:53:43 +0200

    parser update

--- olav/pick.py
+++ olav/pick.py
@@ -4,12 +4,25 @@ BEST_RESULT = {}
 BEST_MAP = 0
 BEST_MRR = 0
 BEST_P10 = 0
+BEST_TWO = 0
 
 
 def compare_all(a, b):
     if b:
         cont = a[list(a)[0]]
         best = b[list(b)[0]]
+        for new, old in zip(cont, best):
+            if old > new:
+                return b
+        return a
+    else:
+        return a
+
+
+def compare_two(a, b):
+    if b:
+        cont = a[list(a)[0]][:2]
+        best = b[list(b)[0]][:2]
         for new, old in zip(cont, best):
             if old > new:
                 return b
@@ -34,11 +47,14 @@ def isfloat(value):
 
 
 def read_data():
-    global BEST_RESULT, BEST_MRR, BEST_MAP, BEST_P10
+    global BEST_RESULT, BEST_MRR, BEST_MAP, BEST_P10, BEST_TWO
     with open(RESULTS_FILE, 'r') as rfile:
+        print('Parsing {}...'.format(RESULTS_FILE))
         lines = [x.strip('\n') for x in rfile.readlines()]
+        line_count = 0
         params = []
         for line in lines:
+            line_count += 1
             temp_res = {}
             if line.startswith('SCR'):
                 elements = line.replace(',', '=').split('=')  # splits the line so it's easy to extract the parameters
@@ -47,16 +63,20 @@ def read_data():
                 elements = line.split('  ')
                 temp_res[elements[0]] = [elements[i] for i in range(1, 4)]
                 temp_res['params'] = ['{:05.3f}'.format(param) for param in params]
-                BEST_RESULT = compare_all(temp_res, BEST_RESULT)
-                BEST_P10 = compare_one(temp_res[elements[0]][0], BEST_P10)
-                BEST_MAP = compare_one(temp_res[elements[0]][1], BEST_MAP)
-                BEST_MRR = compare_one(temp_res[elements[0]][2], BEST_MRR)
+                BEST_RESULT, BEST_TWO = compare_all(temp_res, BEST_RESULT), compare_two(temp_res, BEST_TWO)
+                BEST_P10, BEST_MAP, BEST_MRR = compare_one(temp_res[elements[0]][0], BEST_P10), \
+                                               compare_one(temp_res[elements[0]][1], BEST_MAP), \
+                                               compare_one(temp_res[elements[0]][2], BEST_MRR)
+
+        print('{} lines parsed.'.format(line_count))
 
     return lines
 
 if __name__ == '__main__':
     read_data()
-    print('Best:\nResult: {0}\nP@10: {1}\nMAP: {2}\nMRR: {3}'.format(BEST_RESULT, BEST_P10, BEST_MAP, BEST_MRR))
+    print('Best:\nResult: {0}\nP@10: {1}\nMAP: {2}\nMRR: {3}\nWeighted: {4}'.format(BEST_RESULT, BEST_P10,
+                                                                                    BEST_MAP, BEST_MRR, BEST_TWO))
     with open(WINNER_FILE, 'w') as wfile:
-        print('Writing results to file.')
-        wfile.write('Result: {0}\nP@10: {1}\nMAP: {2}\nMRR: {3}'.format(BEST_RESULT, BEST_P10, BEST_MAP, BEST_MRR))
+        print('Writing results to {}'.format(WINNER_FILE))
+        wfile.write('Result: {0}\nP@10: {1}\nMAP: {2}\nMRR: {3}\nWeighted: {4}'.format(BEST_RESULT, BEST_P10,
+                                                                                       BEST_MAP, BEST_MRR, BEST_TWO))
